ba16ee9072df1711865dec119433380ba951ad6f
kainbryanjones.personal@gmail.com <Kain Bryan-Jones>
2022-12-30 16:06:23 +0000

    Added content

--- src/App.js
+++ src/App.js
@@ -4,20 +4,65 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{
+        height:"100vh",
+        width: "100vw",
+        backgroundColor: "salmon",
+        display: "flex",
+        justifyContent: "space-evenly",
+        alignItems: "left",
+        flexWrap: "wrap",
+        
+      }}>
+        <div style={{maxWidth:"100%",
+                    backgroundColor:"lightsalmon",
+                    padding:"1.5%"
+      }}>
+          <img
+            src={"https://media.licdn.com/dms/image/D4D03AQH8LWTcO42vDw/profile-displayphoto-shrink_800_800/0/1665586590062?e=1677715200&v=beta&t=JkMAsNzIfoPOXvSu-MOzpVyLGIHVQ88uMrzEM6IO8O0"}
+            style={{
+              maxWidth: "50%",
+              borderRadius: "100%",
+            }}
+          />
+        </div>
+        <div>
+          <h1>Kain Bryan-Jones</h1>
+          <div style={{
+            fontFamily:"initial",
+               }}>
+          <p>
+            Enthusiastic frontend development with skills in application development, music production and animation. Looking to get my foot in the door of the music software industry.<br/>
+            As a young musician myself, I have experience within the world of music production including the tools used to help facilitate it. With this personal experience and my background in software development, I have a strong desire to build quality products to assist other musicians in their own journey, whether they want to be a casual four-chord player or a professional multi-instrumentalist.
+          </p>
+          <p>
+            This website is a work in progress while I make it look prettier.
+          </p>
+          </div>
+        </div>
+        <div className='menu-index'>
+          <ul>
+            <li>Projects
+              <ul>
+                <li>
+                  <a href="www.halogensband.com">Halogens Website</a>
+                </li>
+                <li>
+                  <a href="">Captain Mercy Album</a>
+                </li>
+                <li>
+                  <a href="https://www.youtube.com/watch?v=jcC4XfAb8ZM">Motorbike Official Visualiser</a>
+                </li>
+                <li>
+                  <a href="https://github.com/kainbryanjones/Mercy">Mercy</a>
+                </li>
+              </ul>
+            </li>
+            <li><a href="/cv.pdf">CV</a></li>
+            <li><a href="mailto: contact.kainbryanjones@gmail.com">Contact</a></li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
