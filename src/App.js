272640b0c47c95601a1423cc3b28513383f0208f
Kain Bryan-Jones <89807193+kainbryanjones@users.noreply.github.com>
2023-01-02 06:18:13 +0000

    Fixed grammar error

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
           <main>
             <div className='main-content'>
               <p>
-                Enthusiastic frontend development with skills in application development, music production and animation. Looking to get my foot in the door of the music software industry.<br />
+                Enthusiastic frontend developer with skills in application development, music production and animation. Looking to get my foot in the door of the music software industry.<br />
                 As a young musician myself, I have experience within the world of music production including the tools used to help facilitate it. With this personal experience and my background in software development, I have a strong desire to build quality products to assist other musicians in their own journey, whether they want to be a casual four-chord player or a professional multi-instrumentalist.
               </p>
             </div>
