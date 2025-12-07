2f18a63a855bc7dc81b0e692a2af03cb0b5f06b2
Kain Bryan-Jones <89807193+kainbryanjones@users.noreply.github.com>
2025-12-07 01:24:23 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,30 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div style={{
-        minWidth:"100vw",
-        minHeight:"100vh",
-        backgroundColor: "#7fffd4",
-        display: "flex",
-        justifyContent: "space-evenly",
-        alignItems: "left",
-        flexWrap: "wrap",
-
-      }}/>
+      <>
+      <div
+        style={{
+          minWidth: "100vw",
+          minHeight: "100vh",
+          background: "linear-gradient(270deg, #7fffd4, #40e0d0, #7fffd4)",
+          backgroundSize: "600% 600%",
+          animation: "gradientAnimation 10s ease infinite",
+          display: "flex",
+          justifyContent: "space-evenly",
+          alignItems: "flex-start",
+          flexWrap: "wrap",
+        }}
+      />
+      <style>
+        {`
+          @keyframes gradientAnimation {
+            0% { background-position: 0% 50%; }
+            50% { background-position: 100% 50%; }
+            100% { background-position: 0% 50%; }
+          }
+        `}
+      </style>
+    </>
   </div>
   );
 }
